055444d0d9d3a8b7f2f2767e07dd4e846bc26360
cyril.bazin <cyril.bazin@063941aa-af05-11dd-af79-39f66dce3c3c>
2009-04-10 14:36:40 +0000

    The grammar parser works The grammar to parse parsers rocks

--- trunk/parthon.test.py
+++ trunk/parthon.test.py
@@ -1,5 +1,40 @@
 from parthon import *
-        
+import UserList       
+ 
+class ListElem:#(UserList.UserList):
+    
+    def __init__(self):
+        self._elem = None
+        self._rest = None
+        self._nbElems = 0
+
+    def cloneAndAppend(self, newElem):
+        newList = ListElem()
+        newList._elem = newElem
+        newList._rest = self
+        newList._nbElems = self._nbElems + 1
+        return newList
+
+    def __iter__(self):
+        current = self
+        while current._rest is not None:
+            yield current._elem
+            current = current._rest 
+
+    def __len__(self):
+        return self._nbElems
+    
+    def __getitem__(self, i):
+        if i < 0: 
+            i = self._nbElems - i 
+        for j, elem in self:
+            if j == i: 
+                return elem
+        raise Exception("index out of range")
+
+    def __repr__(self):
+        return "<<"+ str(list(self)) +">>"
+
 class Grammar:        
       
   def parser_(self, x):
@@ -10,30 +45,41 @@ class Grammar:
     return FctParser(getParser)
   
   def text_(self, x):
-    print "text :", x
     return txt(x)
   
-  def notFuture_(self, a):
+  def notInFuture_(self, a):
     return NoFutureCheckerParser(a)
   
   def nothing_(self):    
       return nothing()  
 
+  def anything_(self):
+      return item
+
   def maybe_(self, a):
     return a | nothing()
   
   def many0_(self, a):
+    return maxi0(a, ListElem.cloneAndAppend, ListElem())
     return maxi0(marked(a), concat2, ())
   
   def many_(self, a):
+    return maxi(a, ListElem.cloneAndAppend, ListElem())
     return maxi(marked(a), concat2, ())
   
   def future_(self, a):
     raise NotImplementedError    
     
   def addParser_(self, name, parser):
-    self._lstParsers[name] = parser 
+    #print "New Parser :", name
+    self._lstParsers[name] = parser
     return (name, parser)
+
+  def bind_(self, parser, method):
+    if method is not None:
+        method = getattr(self._object, method)
+        parser = ConvertParser(parser, method)
+    return parser
 
   def conjonction_(self, a, b=None):
     if b is None:
@@ -45,8 +91,15 @@ class Grammar:
       return a
     return a | b
 
-  def __init__(self):
+  def setBinder(self, obj):
+    self._object = obj
+
+  def initVals(self, obj):
+    self._object = obj
     self._lstParsers = {}
+
+  def __init__(self, obj=None):
+    self.initVals(obj)
     def suite(pa, fct):
       def B(a):
         return (((ConstParser(a)["a"] + pa["b"]) >= fct)["a"] / B) | nothing(a)
@@ -67,25 +120,32 @@ class Grammar:
       return  pa["a"] / B 
       
     #(inList("\"\\/bfnrt"))["a"] >= iden) 
-     
+    
     def debug(a):
-        print "###", a
+        print "Debug :", a
         return a
     
     def pureText():
-        charText = (txt("\\\"", "\"") | txt("\\\\", "\\") | notInList("\\\""))
+        def isBasic(c):
+            if c == '"':
+                return Failure
+            return c
+        basicCharText = item["c"] >= isBasic
+        charText = (txt('\\"', '"') | txt('\\\\', '\\') | basicCharText )
         return (lit('"') >> manyChars(charText)["a"] >> lit('"') >= iden)
 
     parserName = word
+    methodName = maxiChars(letter|lit("_"))
     
     errorAtom = error(2, "An atom is expected there")
       
     def fct_atom():
       return (
-             (~lit('(') - expr["a"] - ~lit(')') >= iden)  |
+             (lit('(') - expr["a"] - lit(')') >= iden)  |
              (pureText()["x"]        >= self.text_) |
              (parserName["x"]      >= self.parser_) | 
-             (lit("_")             >= self.nothing_) |
+             (lit("!")             >= self.nothing_) |
+             (lit("%")             >= self.anything_) |
              errorAtom)
              
             
@@ -93,75 +153,272 @@ class Grammar:
     postFixedAtom = postfix(atom, [(lit("?"), self.maybe_),
                                    (lit("*"), self.many0_),
                                    (lit("+"), self.many_),
-                                   (lit("!"), self.notFuture_),
+                                   (lit("!"), self.notInFuture_),
                                    (lit("&"), self.future_),
                                   ]
                                )
     #fact = operation_la(atom, power)
     #exprAnd = suite(postFixedAtom, self.conjonction_)
-    exprAnd = operation_la(postFixedAtom, optSpaced(lit(",")), self.conjonction_)
+    bind = ((txt("=>") - methodName["a"] >= iden) | ConstParser(None))
+    bindedAtom = postFixedAtom["parser"] - bind["method"] >= self.bind_ 
+
+    exprAnd = operation_la(bindedAtom, optSpaced(lit(",")), self.conjonction_)
     exprOr  = operation_la(exprAnd, optSpaced(lit("|")), self.disjonction_)
 
     expr = exprOr
     
-    assignment = word["name"] - txt("<-") - expr["parser"] - lit(";") >= self.addParser_
+
+    assignment = (word["name"] + txt(":") - expr["parser"] - lit(";") >= self.addParser_)
 
     #self._grammarParser = (optSpaced(expr["a"]) >> eot) >= iden
     
     def get_a(a, b): 
         return a 
 
-    self._grammarParser = optSpaced(suite(assignment, get_a))#self._lstParsers["Main"]   
+    self._grammarParser = optSpaced(suite(assignment, get_a)) |error(5, "parse error")#self._lstParsers["Main"]   
 
   def getMain(self):
-    for k, v in self._lstParsers.iteritems():
-        print k, v
-    return self._lstParsers["Main"]["a"] >> eot >= iden
+    #print "Liste des parseurs connus : "
+    #for k, v in self._lstParsers.iteritems():
+    #    print ">", k, v
+    main = self._lstParsers["Main"]
+    return (filterList(main["a"] >> ~eot) | error(4, "x"))
     
   def parse(self, text):
-    print text
-    try:
-      parseResult = parse(self._grammarParser, text)
-    except ParseError, e:
-      print e
+    parseResult = parse(self._grammarParser, text)
     for n, ares in enumerate(parseResult):
       r, d, dic = ares
-      print "/////////////////////////////////////////"
       if r:
-        print r
-        #print "res (%2d): %s, %s, %s" % (n, r, repr(''.join(d)), dic)
-        #print "++++++++"
-        #print r.getValue()
-        #print "++++++++"
-        #print r.getValue().asTree()
-        #print "--------"
-        #print r.getValue().simplify().asTree()
-        #print "--------"
-        break          
+        return self.getMain()
     else: 
         print "PARSE ERROR"
+
+
+def filterList(p):
+    def list2item(l):
+        return l[0]
+    return ConvertParser(p, list2item)
+    
+
+class Binder:
+    def __init__(self):
+        self._level = 1
+
+    def isChar(self, items):
+        if not items.isalpha():
+            return Failure
+        return items
+
+    def filter(self, items):
+        return NoResult
+
+    def start(self, items):
+        self._level = 0
+        return NoResult
+
+    def text(self, items):
+        if self._level == 0: 
+            return "".join(l[0] for l in items[0])
+        return NoResult
+    
+    def open(self, items):
+        self._level += 1
+        return NoResult
+    
+    def close(self, items):
+        self._level -= 1
+        return NoResult
+
+class AnotherGrammar(Grammar):
+  def __init__(self):
+    self.initVals(self)
+
+  def debug(self, items):
+    print "===>", "".join(items[0])
+    return items
+
+  def filter(self, items):
+    return NoResult
+
+  def isChar(self, items) : 
+    if items == "_":
+      return items
+    if items.isalpha():
+      return items
+    return Failure
+
+  def isValidTextChar(self, items):
+    if items == '"':
+        return Failure
+    return items
+
+  def isSpaceChar(self, items) :
+    if not items.isspace():
+        return Failure
+    return items
+
+  def toString(self, items):
+    return "".join(items)
    
-        
-def main():            
-  #text = '"a?\\"bba"|(b?|c     c)|"aa"|"bb"'
+  def escapedQuote(self, items):
+    return items[1]
+
+  def postFixMaybe(self, items):
+    return self.maybe_
+
+  def postFixMany(self, items):
+    return self.many_
+
+  def postFixManyOrZero(self, items):
+    return self.many0_
+
+  def postFixInFuture(self, items):
+    return self.inFuture_
+
+  def postFixNotInFuture(self, items):
+    return self.notInFuture_
+
+  def parserAnything(self, items):
+    return self.anything_()
+
+  def parserNothing(self, items):
+    return self.nothing_()
+
+  def parserExprParenthesed(self, items):
+    return items[1]
+
+  def parserText(self, items):
+    return txt("".join(items[1]))
+
+  def parserBindedAtom(self, items):
+    parser = items[0]
+    method = items[-1]
+    if method is not None:
+      return self.bind_(parser, method[1])
+    return parser
+
+  def parserGetParser(self, items):
+    return self.parser_(items)
+
+  def parserDisjonction(self, items):
+    firstParser = items[0]
+    if not items[1]:
+        return firstParser
+    others = list(parser[1] for parser in items[1])
+    return DisjonctionParser([firstParser]+others)
+
+  def parserConjonction(self, items):
+    firstParser = items[0]
+    if not items[1]:
+        return firstParser
+    others = list(parser[1] for parser in items[1])
+    return ConjonctionParser([firstParser]+others)
+
+  def parserPostFixedAtom(self, items):
+    atom = items[0]
+    for modificator in items[-1]:
+         atom = modificator[0](atom)
+    return atom
+
+  def _parserPostFixedAtom(self, items):
+    atom, modificator = items   
+    print "!!!", items
+    return modificator(atom)
+
+  def newParser(self, items):
+    name, _, parser, _ = items
+    return self.addParser_(name, parser)
+  
+  def theEnd(self, items):
+    self._grammarParser = items
+ 
+
+def main():           
   text = """
-    Begin <- "(*";
-    End <- "*)";
-    Z <- "a"|"bb"|"b";
-    N <- C|(Begin!, End!, Z);
-    C <- Begin, N*, End; 
-    Main <- N*; 
+    S           : %       => isSpaceChar;
+    Char        : %       => isChar;
+    ParserName  : Char+   => toString;
+    MethodName  : Char+   => toString;
+    Ss          : S*      => filter;
+    Anything    : "%"     => parserAnything;
+    Nothing     : "!"     => parserNothing;
+    TextSep     : "\\"";
+    TextChar    : %       => isValidTextChar;
+    EscapedSep  : "\\\\"" => escapedQuote;
+    Maybe       : "?"     => postFixMaybe;
+    Many        : "+"     => postFixMany;
+    ManyOrZero  : "*"     => postFixManyOrZero;
+    InFuture    : "&"     => postFixInFuture;
+    NotInFuture : "!"     => postFixNotInFuture;
+    OpAnd       : (Ss, ",", Ss);
+    OpOr        : (Ss, "|", Ss);
+    OpBind      : (Ss, "=>", Ss);
+    ExprParenthesed : ("(", Ss, Expr, Ss, ")")                 => parserExprParenthesed;
+    Text        : (TextSep, (EscapedSep | TextChar)*, TextSep) => parserText;
+    PostFix     : Maybe|Many|ManyOrZero|InFuture|NotInFuture ;
+    Parser      : ParserName                                   => parserGetParser;
+    Atom        : Parser | Text | Nothing | Anything | ExprParenthesed ;
+    PostFixedAtom : (Atom, (Ss, PostFix)*)                     => parserPostFixedAtom;
+    BindedAtom  : (PostFixedAtom, (OpBind, MethodName)?)       => parserBindedAtom;
+    ExprAnd     : (BindedAtom, (OpAnd, BindedAtom)*)           => parserConjonction;
+    ExprOr      : (ExprAnd, (OpOr, ExprAnd)*)                  => parserDisjonction;
+    Expr        : ExprOr ;
+    Line        : (ParserName, Ss, ":", Ss, Expr, Ss, ";")     => newParser;
+    Grammar     : (Ss, Line)*, Ss;
+    Main        : Grammar                                      => theEnd;
+  """
+
+  def run(parser, text):
+    result = parse(parser, text)
+    for n, ares in enumerate(result):
+      r, d, dic = ares
+      print "Result found!"
+      if r:
+        return r
+    else:
+      print "PARSE ERROR"
+    
+  grammar1 = Grammar()
+  grammar2 = AnotherGrammar()
+  grammar3 = AnotherGrammar()
+  grammar4 = AnotherGrammar()
+
+  print "Original parser"
+  grammar1.setBinder(grammar2)
+  parser2 = grammar1.parse(text)
+
+  print "Original parser (simplified)"
+  grammar1._grammarParser.simplify()
+  parser2 = grammar1.parse(text)
+
+  print "Parsed parser (simplified)"
+  grammar2.setBinder(grammar3)
+  grammar2._grammarParser = parser2
+  grammar2._grammarParser.simplify()
+  parser3 = grammar2.parse(text)
+ 
+  print "Parsed parser (simplified)"
+  grammar3.setBinder(grammar4)
+  grammar3._grammarParser = parser3
+  grammar3._grammarParser.simplify()
+  parser4 = grammar3.parse(text)
+ 
+
+  print "TEST : -------------------------------------------------"
+  text2 = """
+    Begin  : "(*"                ;         
+    End    : "*)"                ;
+    Z      : %                    => isChar;
+    N      : C  | (Begin!, End!, Z);           
+    C      : (Begin => open , N*, End => close) => filter     ;
+    Main   :  ((! => start), N*)                 => text; 
   """   
-  #text = """
-  #  Main <- "a";
-  #""" 
-  g = Grammar()
-  print g._grammarParser.asTree()
-#  print repr(g._grammarParser)
-#  print g._grammarParser.asText()
-  #try:
-  g.parse(text)
-  p = g.getMain()
+  grammar4.setBinder(Binder())
+  grammar4._grammarParser = parser4
+  grammar4._grammarParser.simplify()
+  
+  
+  p = grammar4.parse(text2)
   print "++++++++"
   print p
   print "++++++++"
@@ -169,19 +426,11 @@ def main():
   print "--------"
   print p.simplify().asTree()
   print "--------"
- 
-  text = "a(*abababb(*b*)bbb*)" 
-  parseResult = parse(p, text)
   
-  for n, ares in enumerate(parseResult):
-    r, d, dic = ares
-    print "/////////////////////////////////////////"
-    if r:
-      print r
-      break
-    else:
-        print "PARSE ERROR"
-  else: 
-    print "ERROR parsing <%s>"% text
-  
+  text3 = "bab(*b(*ba*)b*)b(**)a"*100 
+  print "Input :", text3
+  result =parse(p, text3)
+  res = result.next()[0].getValue() 
+  print "Output :", res
+
 main()
